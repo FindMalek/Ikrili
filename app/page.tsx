4234122a71e26661b3640f8f6c7e4e3b3a4e5b5b
FindMalek <malekgarahellalbus@gmail.com>
2023-08-09 12:34:06 +0100

    (0.1.2) - Section added 'herosection' | Home Page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import Navbar from '@/components/essentials/Navbar'
-
+import Herosection from '@/components/home-page/Herosection'
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Herosection />
     </div>
   )
 }
